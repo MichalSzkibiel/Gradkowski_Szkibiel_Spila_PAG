30d75b86e09cf9e53caa53be97875e30110f55e9
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2017-11-30 18:03:53 +0100

    wizualizacja.py
    Poprawione zapytanie SQL  Zmiana nazw zmiennych i dodanie nowej

--- wizualizacja.py
+++ wizualizacja.py
@@ -1,6 +1,11 @@
 import arcpy
-def wizualizacja (tablica, drogi,result):
+def wizualizacja (drogi, tab_layer,result):
     arcpy.MakeFeatureLayer_management(drogi,tab_layer)
-    SelectLayerByAttribute_management(tab_layer,"NEW_SELECTION","","","FID IN (SELECT FID FROM tablica)")#FID znajduje się w tablicy "tablica"
+    SelectLayerByAttribute_management(tab_layer,"NEW_SELECTION","FID IN (FID)")#FID znajduje się w tablicy "drogi"
     arcpy.CopyFeatures_management(tab_layer,result)
 
+#Okreslenie nazwy layera
+tab_layer = "trasa_lyr"    
+#Wywolanie we skrypcie find_path    
+#Zamiana w Shapefile
+result=wizualizacja(roads,tab_layer,file)
